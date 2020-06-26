5ce3e7b065c3ea2f1e7db3c01c5b403761e6d5db
Nilanjan <Nilanjan@DESKTOP-37EPDOT>
2020-06-26 10:54:02 +0530

    Updated the code based on your comment. Please Approve.

--- src/test/java/GitTest/GitTest/Test2.java
+++ src/test/java/GitTest/GitTest/Test2.java
@@ -1,8 +1,9 @@
 package GitTest.GitTest;
 
-public class test2 {
+public class Test2 {
 	public static void main(String[] arg) {
 		System.out.println("Hello World saying Nilanjan.");
+		System.out.println("Updated the code as suggested.");
 	}
 
 }
